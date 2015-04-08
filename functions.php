9a3816cf5cb46dda94a0ae0a7df0db083d309f3f
Dragos BUBU <i_am_bubudragos@yahoo.com>
2015-04-08 13:56:09 +0300

    Final considerations
    + replaced function names
    + added image source in readme

--- functions.php
+++ functions.php
@@ -1,25 +1,18 @@
 <?php
 // add any new or customised functions here
-	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-	function theme_enqueue_styles() {
-		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('flatx_bootstrap_style') );
-		// Loads our main stylesheet.
-		wp_enqueue_style( 'flatx-child-style', get_stylesheet_uri(), array('flatx_style'), 'v1' );
-	}
-	function remove_style_child(){
-		remove_action('wp_print_scripts','flatx_php_style');	
-	}
-	add_action( 'wp_enqueue_scripts', 'remove_style_child', 100 );
+add_action( 'wp_enqueue_scripts', 'flat_boat_enqueue_styles' );
+function flat_boat_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	// Loads our main stylesheet.
+	wp_enqueue_style( 'flat_boat-child-style', get_stylesheet_uri(), array('flat-style') );
+}	
 	
-	
-if ( ! function_exists( 'flat_boat_setup1' ) ) :
+if ( ! function_exists( 'flat_boat_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 */
-	function flat_boat_setup1() {
+	function flat_boat_setup() {
 	
-		# Enable WordPress theme features
-		add_theme_support( 'automatic-feed-links' ); # @link http://codex.wordpress.org/Automatic_Feed_Links
 		$custom_background_support = array(
 			'default-color'          => '',
 			'default-image'          => get_stylesheet_directory_uri() . '/assets/img/default-background.jpg',
@@ -31,4 +24,4 @@ if ( ! function_exists( 'flat_boat_setup1' ) ) :
 
 	}
 endif;
-add_action( 'after_setup_theme', 'flat_boat_setup1' );
+add_action( 'after_setup_theme', 'flat_boat_setup' );
